a1318517430579b072ea3588afc4a7de16ab524d
Rewalon <rewalon@subreal-team.com>
2021-02-15 19:03:52 +0500

    6 lessons

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class ScreenWidget extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Center(child: const Text('Counter')),
+          title: Center(child: const Text('Колонки и столбцы')),
         ),
         body: SimpleWidget(),
       ),
@@ -16,38 +16,59 @@ class ScreenWidget extends StatelessWidget {
   }
 }
 
-class SimpleWidget extends StatefulWidget {
+class TextWidget extends StatelessWidget {
+  const TextWidget({Key key, this.text, this.color, this.width, this.fontSize})
+      : super(key: key);
+
+  final String text;
+  final Color color;
+  final double width;
+  final double fontSize;
+
   @override
-  _SimpleWidgetState createState() => new _SimpleWidgetState();
+  Widget build(BuildContext context) {
+    return Container(
+        width: width,
+        color: color,
+        height: 100,
+        child: Text(
+          text,
+          style: TextStyle(fontSize: fontSize),
+        ));
+  }
 }
 
-class _SimpleWidgetState extends State<SimpleWidget> {
-  int _count = 0;
-
-  void _handleButton() {
-    setState(() {
-      _count++;
-    });
-  }
-
+class SimpleWidget extends StatelessWidget {
   @override
-  // TODO: implement widget
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('$_count'),
-          RaisedButton(
-            onPressed: () {
-              _handleButton();
-            },
-            child: Text('ֽНу зачем иврит то :)'),
-          )
-        ],
-      ),
-    );
     // TODO: implement build
-    throw UnimplementedError();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      textBaseline: TextBaseline.alphabetic,
+      textDirection: TextDirection.rtl,
+      verticalDirection: VerticalDirection.up,
+      children: <Widget>[
+        TextWidget(
+          color: Colors.limeAccent,
+          width: 110,
+          text: 'Первый',
+          fontSize: 20,
+        ),
+        TextWidget(
+          color: Colors.cyanAccent,
+          width: 140,
+          text: 'Второй',
+          fontSize: 30,
+        ),
+        TextWidget(
+          color: Colors.purpleAccent,
+          width: 100,
+          text: 'Третий',
+          fontSize: 30,
+        ),
+      ],
+    );
   }
 }
